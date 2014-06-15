5134e91ba1ad5a3691418dfa9eda21c8bb64123c
Marek Jędryka <marek629@poczta.onet.pl>
2014-06-15 15:03:32 +0200

    First GitHub Pages commit.

---
+++ blog.js
@@ -0,0 +1,28 @@
+var blogList = [
+    {   'date': new Date(2014, 4, 31),
+        'file': '_release_2.7.2.html',
+        'author': 'marek'
+    },
+    {   'date': new Date(2014, 3, 24),
+        'file': '_installer_2.7.1.html',
+        'author': 'marek'
+    },
+    {   'date': new Date(2014, 3, 17),
+        'file': '_release_2.7.1.html',
+        'author': 'marek'
+    },
+    {   'date': new Date(2014, 3, 10),
+        'file': '_new_site.html',
+        'author': 'marek'
+    }
+//     {   'date': new Date(2014, 1, 28),
+//         'file': '_release_2.7.html'
+//     }
+]
+
+var monthName = [
+    'January', 'February', 'March',
+    'April', 'May', 'June',
+    'July', 'August', 'September',
+    'October', 'November', 'December'
+];
