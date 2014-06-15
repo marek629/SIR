087e33bf503a4f01d9a3fdbe3420287786d3c6df
Marek Jędryka <marek629@poczta.onet.pl>
2014-06-15 15:44:39 +0200

    Removed '_' character from file names.

--- blog.js
+++ blog.js
@@ -1,18 +1,18 @@
 var blogList = [
     {   'date': new Date(2014, 4, 31),
-        'file': '_release_2.7.2.html',
+        'file': 'release_2.7.2.html',
         'author': 'marek'
     },
     {   'date': new Date(2014, 3, 24),
-        'file': '_installer_2.7.1.html',
+        'file': 'installer_2.7.1.html',
         'author': 'marek'
     },
     {   'date': new Date(2014, 3, 17),
-        'file': '_release_2.7.1.html',
+        'file': 'release_2.7.1.html',
         'author': 'marek'
     },
     {   'date': new Date(2014, 3, 10),
-        'file': '_new_site.html',
+        'file': 'new_site.html',
         'author': 'marek'
     }
 //     {   'date': new Date(2014, 1, 28),
