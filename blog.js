8fa9fcbff4b0f5d20ea7b965092d745026e0b055
Marek Jędryka <marek629@poczta.onet.pl>
2014-08-26 20:13:14 +0200

    Blog logic code moved from sir.js to blog.js file.

--- blog.js
+++ blog.js
@@ -1,3 +1,5 @@
+// Blog Data
+
 var blogList = [
     {   'date': new Date(2014, 7, 24),
         'file': 'release_2.7.3.html',
@@ -43,3 +45,74 @@ var monthName = [
     'July', 'August', 'September',
     'October', 'November', 'December'
 ];
+
+// Blog Logic
+
+var getEntryPage = function(entry) {
+    $.ajax({
+        type: 'GET',
+        async: false,
+        url: entry.file,
+        dataType: 'text',
+        success: function (content) {
+            $('#content').append(content);
+            $('#content .entry-date').last().text(
+                monthName[entry.date.getMonth()] + ' ' + entry.date.getDate() + ', ' + entry.date.getFullYear());
+        }
+    });
+};
+
+var showBlog = function() {
+    $('#content').empty();
+
+    var yearArray = new Array();
+    var activeYear = 0;
+
+    var hash = window.location.hash.replace('#', '');
+    if (hash.length == 0)
+        activeYear = blogList[0].date.getFullYear();
+    else {
+        var int = parseInt(hash);
+        if (isNaN(int))
+            activeYear = blogList[0].date.getFullYear();
+        else
+            activeYear = int;
+    }
+
+    $.each(blogList, function(i, entry) {
+        var year = entry.date.getFullYear();
+
+        if ($.inArray(year, yearArray) < 0) {
+            yearArray.push(year);
+            $('#menu-box').append(
+                '<li>' +
+                    '<a href="#' + year + '">' + year + '</a>' +
+                '</li>'
+            );
+        }
+
+        if (year == activeYear)
+            getEntryPage(entry);
+    });
+
+    $('#menu-box li:contains("' + activeYear + '")').first().addClass('active');
+};
+
+var compareBlogEntry = function(a, b) {
+    return b.date.valueOf() - a.date.valueOf();
+};
+
+$().ready( function() {
+    $.pathchange.init();
+
+    blogList.sort(compareBlogEntry);
+
+    $('#content').empty();
+    showBlog();
+});
+
+$(window).bind('pathchange', function() {
+    $('#content').empty();
+    $('#menu-box').empty();
+    showBlog();
+});
