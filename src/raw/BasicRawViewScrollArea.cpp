809e7fc20dfad3ab29775dc0455aeaadf4c1d254
Marek Jędryka <marek629@poczta.onet.pl>
2015-09-13 22:13:12 +0200

    BasicRawViewScrollArea class was added.

---
+++ src/raw/BasicRawViewScrollArea.cpp
@@ -0,0 +1,94 @@
+/* This file is part of SIR, an open-source cross-platform Image tool
+ * 2007-2010  Rafael Sachetto <rsachetto@gmail.com>
+ * 2011-2015  Marek Jędryka   <jedryka89@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * Program URL: http://marek629.github.io/SIR/
+ */
+
+#include "raw/BasicRawViewScrollArea.hpp"
+
+#include "raw/RawController.hpp"
+
+
+BasicRawViewScrollArea::BasicRawViewScrollArea(QWidget *parent)
+    : QScrollArea(parent), RawView()
+{
+    ui.setupUi(this);
+}
+
+BasicRawViewScrollArea::~BasicRawViewScrollArea() {}
+
+QWidget *BasicRawViewScrollArea::qWidget()
+{
+    return ui.scrollAreaWidgetContents;
+}
+
+bool BasicRawViewScrollArea::isEnabledChecked() const
+{
+    return ui.rawCheckBox->isChecked();
+}
+
+void BasicRawViewScrollArea::setEnabledChecked(bool enabled)
+{
+    ui.rawCheckBox->setChecked(enabled);
+}
+
+QString BasicRawViewScrollArea::pathText() const
+{
+    return ui.dcrawLineEdit->text();
+}
+
+void BasicRawViewScrollArea::setPathText(const QString &text)
+{
+    ui.dcrawLineEdit->setText(text);
+}
+
+void BasicRawViewScrollArea::setPathTextEnabledInput(bool inputEnabled)
+{
+    ui.dcrawLineEdit->setEnabled(inputEnabled);
+}
+
+QString BasicRawViewScrollArea::optionsText() const
+{
+    // TODO: build result string from UI widgets
+    return QString();
+}
+
+void BasicRawViewScrollArea::setOptionsText(const QString &text)
+{
+    // TODO: set UI widgets values from input text string
+}
+
+void BasicRawViewScrollArea::setOptionsTextEnabledInput(bool inputEnabled)
+{
+    ui.interpolationGroupBox->setEnabled(inputEnabled);
+}
+
+void BasicRawViewScrollArea::setHelpTextPlain(const QString &text)
+{
+    Q_UNUSED(text);
+}
+
+void BasicRawViewScrollArea::setButtonEnabledInput(bool inputEnabled)
+{
+    ui.dcrawPushButton->setEnabled(inputEnabled);
+}
+
+bool BasicRawViewScrollArea::closeWindow()
+{
+    return window()->close();
+}
